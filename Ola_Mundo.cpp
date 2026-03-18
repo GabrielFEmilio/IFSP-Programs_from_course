72ac48db82ae843da33ea3bd71e57dd489d23af9
Gabriel F. Emilio <gabriel.fe026@gmail.com>
2026-03-18 18:43:20 -0300

    Initial commit: this repository will store programs developed during my Engineering course. Some files may be written in Portuguese, as it is the primary language required for the course.

---
+++ Ola_Mundo.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <clocale>
+#include <cstdio>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int n1, n2, opcao; // n1 e n2 são os números, opcao é a escolha do menu
+
+    std::printf("--- CALCULADORA ---\n");
+    
+    std::printf("Digite o primeiro número: ");
+    std::scanf("%d", &n1);
+
+    std::printf("Digite o segundo número: ");
+    std::scanf("%d", &n2);
+
+    // AGORA MOSTRAMOS O MENU
+    std::printf("\nO que você deseja fazer?\n");
+    std::printf("1 - Somar\n");
+    std::printf("2 - Subtrair\n");
+    std::printf("3 - Multiplicar\n");
+    std::printf("4 - Dividir\n");
+    std::printf("Sua opção: ");
+    
+    // ESSA LINHA ABAIXO É A QUE ESTAVA FALTANDO!
+    std::scanf("%d", &opcao); 
+
+    // Agora testamos a OPCAO, e não os números da conta
+    if (opcao == 1) {
+        std::printf("Resultado da Soma: %d\n", n1 + n2);
+    } 
+    else if (opcao == 2) {
+        std::printf("Resultado da Subtração: %d\n", n1 - n2);
+    } 
+    else if (opcao == 3) {
+        std::printf("Resultado da Multiplicação: %d\n", n1 * n2);
+    } 
+    else if (opcao == 4) {
+        if (n2 != 0) {
+            std::printf("Resultado da Divisão: %d\n", n1 / n2);
+        } else {
+            std::printf("Erro: Divisão por zero!\n");
+        }
+    } 
+    else {
+        std::printf("Opção inválida!\n");
+    }
+
+    return 0;
+}
